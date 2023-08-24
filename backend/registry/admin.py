d0b3324021c9bbfb45139c99a8404436d458a025
MironFoerster <miron.foerster@gmx.de>
2023-08-24 12:54:09 +0200

    add models to admin, update models

--- backend/registry/admin.py
+++ backend/registry/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Log, Blog, Comment, MsgTemplate
 
-# Register your models here.
+admin.site.register(Log)
+admin.site.register(Blog)
+admin.site.register(Comment)
+admin.site.register(MsgTemplate)
