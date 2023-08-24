c29bda49741b2a1b771af353a1d4edfbb4c9d38e
MironFoerster <miron.foerster@gmx.de>
2023-08-24 15:06:08 +0200

    add api service, observable typing

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class AuthService {
 
   constructor(private _http: HttpClient, private _router: Router) { }
 
-  loginUser(user: any) {
+  loginUser(user: any): Observable<any> {
     return this._http.post<any>(this._loginUrl, user)
   }
 
