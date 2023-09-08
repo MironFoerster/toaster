5ba77d3acc0480057bfcb31d3def730c17a34ec2
MironFoerster <miron.foerster@gmx.de>
2023-09-08 23:53:59 +0200

    try csrf fix

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -3,7 +3,7 @@
 import os
 from .base import *
 
-ALLOWED_HOSTS = os.environ["ALLOWED_HOSTS"].split(" ")
+ALLOWED_HOSTS = os.environ["ALLOWED_HOSTS"].split(" ").append("https://site--toasterapi--2vd4knhkkhn9.code.run")
 SECRET_KEY = os.environ["SECRET_KEY"]
 DEBUG = os.environ["DEBUG"].lower() == "true"
 DATABASES = {
@@ -21,4 +21,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CORS_ALLOWED_ORIGINS = [
     os.environ["FRONTEND_ORIGIN"],
+    "https://site--toasterapi--2vd4knhkkhn9.code.run"
 ]
