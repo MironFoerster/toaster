a39e354a362fa289aad3df02ca64620a9f412d15
MironFoerster <miron.foerster@gmx.de>
2023-09-22 15:52:15 +0200

    add loader to login

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { LoaderService } from '../services/loader.service';
 
 @Component({
   selector: 'app-login',
@@ -11,9 +12,10 @@ export class LoginComponent {
 
   loginUserData = {username: "", password: ""}
 
-  constructor(private _auth: AuthService, private _router: Router) { }
+  constructor(private _auth: AuthService, private _router: Router, private _loader: LoaderService, private _viewContainer: ViewContainerRef) { }
 
   loginSuccess(res: { token: string; }) {
+    this._loader.endLoading()
     localStorage.setItem('token', res.token);
     this._router.navigate(['stats']);
   }
@@ -23,6 +25,7 @@ export class LoginComponent {
   }
 
   loginUser() {
+    this._loader.startLoading("sende...", this._viewContainer)
     this._auth.loginUser(this.loginUserData)
       .subscribe({next: this.loginSuccess.bind(this), error: this.loginFailure.bind(this)})
   }
