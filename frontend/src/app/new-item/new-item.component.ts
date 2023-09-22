ca190bda35c6461ba97dcab87824bd67ab8c073e
MironFoerster <miron.foerster@gmx.de>
2023-09-22 15:37:49 +0200

    new-item add model, add loader

--- frontend/src/app/new-item/new-item.component.ts
+++ frontend/src/app/new-item/new-item.component.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from '../services/api.service';
+import { ModalService } from '../services/modal.service';
+import { first } from 'rxjs';
+import { LoaderService } from '../services/loader.service';
 
 @Component({
   selector: 'app-new-item',
@@ -10,16 +13,29 @@ import { ApiService } from '../services/api.service';
 })
 export class NewItemComponent {
   itemData = {prep: "", name: ""};
-  constructor(private _api: ApiService, private _router: Router) { }
+  constructor(private _api: ApiService, private _router: Router, private _modal: ModalService, private _loader: LoaderService, private _viewContainer: ViewContainerRef) { }
 
-  submitNewItem() { // TODO: prevent empty
+  submitNewItem() {
     const newItemUrl = "state/newitem/"
     if (this.itemData.name=="") {
       alert("Name für Gegenstand fehlt!")
       return
     }
-    this._api.sendData(newItemUrl, this.itemData)
-    .subscribe({next: res => {console.log(res)}, error: err => {console.log(err)}})
-    this._router.navigate(['..']);
+    this._loader.startLoading("sende...", this._viewContainer)
+    this._api.sendData(newItemUrl, this.itemData).subscribe(res => {
+      this._loader.endLoading()
+      this._router.navigate(['..'])
+    })
   }
+
+  openModal(message: string, action: Function) {
+    this._modal.openModal(message, this._viewContainer).pipe(first()).subscribe({
+      next() {action()}, error() {console.log("error")}, complete() {console.log("complete")}
+    })
+  }
+
+  getItemConfirmMessage() {
+    return `Willst du den Gegestand "...mit ${this.itemData.prep} ${this.itemData.name}..." hinzufügen?`
+  }
+
 }
